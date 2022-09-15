6a1cd060ed7008108504bea4c1b17df6260b723f
snkdrel <ces.zop@gmail.com>
2022-09-15 14:37:27 -0500

    Create bookInstance delete form

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -107,13 +107,35 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = (req, res, next) => {
+  BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec((err, bookinstance) => {
+      if(err) {
+        return next(err);
+      }
+      if(bookinstance == null) {
+        // No results.
+        res.redirect("/catalog/bookinstances");
+      }
+      // Succesful, so render.
+      res.render("bookinstance_delete", {
+        title: "Delete BookInstance",
+        bookinstance: bookinstance,
+      });
+    });
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = (req, res, next) => {
+  BookInstance.findByIdAndRemove(req.body.bookinstanceid, (err) => {
+    if(err) {
+      return next(err);
+    }
+
+    // Success - go to BookInstance list
+    res.redirect("/catalog/bookinstances");
+  });
 };
 
 // Display BookInstance update form on GET.
