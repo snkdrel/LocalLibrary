3a6b048adefb5e698946ea6da1168547ab7ffa8a
snkdrel <ces.zop@gmail.com>
2022-09-19 16:37:26 -0500

    Create update author form

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -53,5 +53,17 @@ AuthorSchema
     return `/catalog/author/${this._id}`;
   });
 
+AuthorSchema
+  .virtual('date_of_birth_yyyy_mm_dd')
+  .get(function() {
+    return DateTime.fromJSDate(this.date_of_birth).toISODate();
+  });
+
+AuthorSchema
+  .virtual('date_of_death_yyyy_mm_dd')
+  .get(function() {
+    return DateTime.fromJSDate(this.date_of_death).toISODate();
+  });
+
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
