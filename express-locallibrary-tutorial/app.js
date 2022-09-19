4ee3e78d977d1b39004ed520ccbb996c86a67c5f
snkdrel <ces.zop@gmail.com>
2022-09-19 17:10:58 -0500

    Added files and changes required for deployment to heroku

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -14,7 +14,8 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://snkdrel:pass123@cluster0.xkkxfc1.mongodb.net/local-library?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://snkdrel:pass123@cluster0.xkkxfc1.mongodb.net/local-library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
